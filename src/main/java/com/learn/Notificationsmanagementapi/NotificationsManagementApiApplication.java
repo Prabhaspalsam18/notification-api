c6c5825da0563eec625cbec5eff960ed1898617e
prabhaspalsam18 <prabhaspalsam18@gmail.com>
2023-02-03 15:24:49 +0530

    Notification api

---
+++ src/main/java/com/learn/Notificationsmanagementapi/NotificationsManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.Notificationsmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/Notificationsmanagementapi/NotificationsManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.Notificationsmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
